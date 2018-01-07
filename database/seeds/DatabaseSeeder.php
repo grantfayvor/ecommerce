1be17621f0ac88e3c14f9b4f668009406a731149
Harrison Favour <fayvorharrison101@gmail.com>
2018-01-07 06:02:03 +0100

    added a seeder for admin account which should be deleted immediately in production

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'Admin Admin',
+            'email' => 'admin@afiammuta.com',
+            'phone_number' => '09198765432',
+            'password' => '$2y$10$20uvTri3uDwjF0FgOKnameTQY.IQvgPIMAe2tXoxEUY/w.wiG8U8a',
+            'admin' => true
+        ]);
         $this->call(CategoriesSeeder::class);
         $this->call(ProductsSeeder::class);
     }
